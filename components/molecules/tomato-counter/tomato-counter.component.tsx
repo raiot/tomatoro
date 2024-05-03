342b5e5107db18e9078b20b6ba60a2d619b93582
tonymtz <atony.mtz@gmail.com>
2024-05-03 12:06:51 -0700

    small label change

--- components/molecules/tomato-counter/tomato-counter.component.tsx
+++ components/molecules/tomato-counter/tomato-counter.component.tsx
@@ -1,5 +1,5 @@
 import useTranslation from 'next-translate/useTranslation'
-import { FC, ReactNode } from 'react'
+import { FC } from 'react'
 import { Button, Card, Flex, Heading, Paragraph } from 'theme-ui'
 
 import { Popup } from '~/components/atoms/popup'
